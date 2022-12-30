9993a708529b9857e3426fc097701e3861c50f9b
Leo <leonardogilli@outlook.com>
2022-12-30 19:49:57 -0300

    room working

--- index.ts
+++ index.ts
@@ -8,7 +8,11 @@ import { Server } from 'socket.io';
 const app = express();
 const port = 3000;
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: '*'
+  }
+});
 
 app.use(cors());
 app.use(express.json());
@@ -19,26 +23,70 @@ app.get('/', (req, res) => {
   res.send('Backend Running');
 });
 
-// Endpoint que sirve un html desde el backend. Esto lo vamos a borrar mas adelante pero para practicar websocket nos viene bien.
-// Si te defendes con vanilla js en el index.html podes meter mano
-app.get('/socket', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+type User = {
+  clientId: string;
+  username: string;
+  card: string | null;
+};
 
-// https://socket.io/get-started/chat
+const rooms: Record<string, User[]> = {};
 
-// Esta es la documentacion con la que hice el tutorial.
-// Fijate que abajo de todo dice Homework
-// Podemos ir viendo eso hasta el martes a modo de practica para ir agarrandole la mano
+io.on('connection', client => {
+  client.on('disconnect', () => {
+    console.log(`Disconnected: ${client.id}`);
+  });
 
-// Conexion socket escucha el evento connection
-io.on('connection', socket => {
-  // Escucha el evento chat message
-  socket.on('chat message', msg => {
-    // Emite un evento chat message que va a ser escuchado por todos, incluso el que lo envia...
-    // Ahora hay que mejorarlo para que escuchen los demas y no el que envia
-    // Si te animas metele ;D
-    io.emit('chat message', msg);
+  client.on('create_room', username => {
+    const newRoom = `${new Date().getTime()}`;
+    rooms[newRoom] = [{ clientId: client.id, username, card: null }];
+
+    client.join(newRoom);
+
+    // console.log('NEW ROOM CREATED', rooms);
+
+    client.emit('new_room', newRoom);
+    client.emit('users', rooms[newRoom]);
+  });
+
+  client.on('join_room', data => {
+    const { room, username } = data;
+
+    if (!rooms[room]) {
+      return;
+    }
+
+    if (rooms[room].some(c => c.clientId === client.id)) {
+      return;
+    }
+
+    rooms[room].push({ clientId: client.id, username, card: null });
+    client.join(room);
+
+    client.broadcast.to(room).emit('users', rooms[room]);
+    client.emit('users', rooms[room]);
+
+    // console.log('ROOM JOINING', rooms);
+  });
+
+  client.on('card', data => {
+    const { card, username, room } = data;
+
+    if (!rooms[room]) {
+      return;
+    }
+
+    const indexEl = rooms[room].findIndex(user => {
+      return user.clientId === client.id;
+    });
+
+    if (indexEl === -1) {
+      return;
+    }
+
+    rooms[room][indexEl].card = card;
+
+    client.broadcast.to(room).emit('users', rooms[room]);
+    client.emit('users', rooms[room]);
   });
 });
 
